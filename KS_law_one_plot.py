f3857123b6ed5a69584df169d84000d1cb4368bf
Eva Franck <Evafranck@gmx.de>
2023-11-19 15:46:20 +0100

    added bigiel law and corrected bins in KS law

--- KS_law_one_plot.py
+++ KS_law_one_plot.py
@@ -15,6 +15,8 @@ KS_gas_dens = []
 KS_star_dens = []
 KS_xsigma = []
 KS_ysigma = []
+KS_xbigiel = []
+KS_ybigiel = []
 
 # calculate SFR with t_ff and epsilon_ff = 0.1%, 1%, 10% 	
 x = np.logspace(-1.5, 1, 100)  # Generates 100 points from 10^0 to 10^2 Msol/pc^2
@@ -22,7 +24,6 @@ G = 6.7*10**(-11)*units.m**3/(units.kg*units.s**2)
 t_ff = np.sqrt(3*np.pi/(32*G.in_units('pc**3 Msol**-1 yr**-2')*x)) # in yr
 SFR = x*10**6/t_ff # in Msol/yr/kpc^2
 
-print(t_ff)
 
 # Create a list of simulation paths
 simulations = ['master', 'semenov', 'evans', 'federrath']
@@ -33,7 +34,7 @@ colorlist = ['blue','orange', 'green', 'red']
 
 # Calculate the Kennicutt-Schmidt law for a given simulation (modified from pynbody documentation)
 def schmidtlaw(sim, filename=None, pretime='50 Myr',
-			   diskheight='3 kpc', rmax='20 kpc', compare=True,
+			   diskheight='2 kpc', rmax='30 kpc', compare=True,
 			   radial=True, bins=10, **kwargs):
 
 	if not radial:
@@ -54,8 +55,9 @@ def schmidtlaw(sim, filename=None, pretime='50 Myr',
 
 	# calculate surface densities
 	if radial:
-		ps = profile.Profile(diskstars[youngstars], nbins=bins)
-		pg = profile.Profile(diskgas, nbins=bins)
+		ps = profile.Profile(diskstars[youngstars], bins=np.linspace(0, 30, 60))
+		pg = profile.Profile(diskgas, bins=np.linspace(0, 30, 60))
+
 	else:
 		# make bins 2 kpc
 		nbins = rmax * 2 / binsize
@@ -70,13 +72,15 @@ def schmidtlaw(sim, filename=None, pretime='50 Myr',
 	gas_dens = pg['density'].in_units('Msol pc^-2')
 	star_dens = ps['density'].in_units('Msol kpc^-2')/pretime/1e6
 
+ 
+
 	if compare:
 		xsigma = np.logspace(np.log10(pg['density'].in_units('Msol pc^-2').min()),
 							 np.log10(
 								 pg['density'].in_units('Msol pc^-2').max()),
 							 100)
-		ysigma = 2.5e-4 * xsigma ** 1.5  # Kennicutt (1998)
-		xbigiel = np.logspace(1, 2, 10)
+		ysigma = 2.5e-4 * xsigma ** 1.4  # Kennicutt (1998)
+		xbigiel = np.logspace(0, 1, 10)
 		ybigiel = 10. ** (-2.1) * xbigiel ** 1.0   # Bigiel et al (2007)
 	
 	return gas_dens, star_dens, xsigma, ysigma, xbigiel, ybigiel
@@ -90,10 +94,12 @@ def KS(mod):
     #disk = f.LowPass('r', '30 kpc') & f.BandPass('z', '-10 kpc', '10 kpc')
     #s = s_all[disk]
     s = s_all
-    KS_gas_dens.append(schmidtlaw(s, pretime = '50 Myr', compare = True, bins = 20)[0])
-    KS_star_dens.append(schmidtlaw(s, pretime = '50 Myr', compare = True, bins = 20)[1])
-    KS_xsigma.append(schmidtlaw(s, pretime = '50 Myr', compare = True, bins = 20)[2])
-    KS_ysigma.append(schmidtlaw(s, pretime = '50 Myr', compare = True, bins = 20)[3])
+    KS_gas_dens.append(schmidtlaw(s, pretime = '50 Myr', compare = True)[0])
+    KS_star_dens.append(schmidtlaw(s, pretime = '50 Myr', compare = True)[1])
+    KS_xsigma.append(schmidtlaw(s, pretime = '50 Myr', compare = True)[2])
+    KS_ysigma.append(schmidtlaw(s, pretime = '50 Myr', compare = True)[3])
+    KS_xbigiel.append(schmidtlaw(s, pretime = '50 Myr', compare = True)[4])
+    KS_ybigiel.append(schmidtlaw(s, pretime = '50 Myr', compare = True)[5])
 
 for sim_path in simulations:
     KS(sim_path)
@@ -104,6 +110,7 @@ fig = plt.figure(figsize=(10, 10))
 for n in range(len(simulations)):
     plt.loglog(KS_gas_dens[n], KS_star_dens[n], "+", label = sim_labels[n], color = colorlist[n])
 plt.loglog(KS_xsigma[0], KS_ysigma[0], label='Kennicutt (1998)')
+plt.loglog(KS_xbigiel[0], KS_ybigiel[0], label='Bigiel et al. (2008)')
 plt.loglog(x, 0.1*SFR, ls = '-', color = 'grey', label = r'10% $\epsilon_{\rm{ff}}$')
 plt.loglog(x, 0.01*SFR, ls = '--', color = 'grey', label = r'1% $\epsilon_{\rm{ff}}$')
 plt.loglog(x, 0.001*SFR, ls = ':', color = 'grey', label = r'0.1% $\epsilon_{\rm{ff}}$')
